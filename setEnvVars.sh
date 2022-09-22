b5d9c52dcd0a6c26cfa8e49102690c76d2185385
Alex Rodriguez <arodriguez@dminc.com>
2022-09-22 14:34:18 -0400

    Commit 2

--- setEnvVars.sh
+++ setEnvVars.sh
@@ -1,3 +1,3 @@
 export GITHUB_USERNAME="arodriguezdev"
-export GITHUB_REPO="bootcamp-2022-sls-api-rodriguez.git"
+export GITHUB_REPO="bootcamp-2022-sls-api-rodriguez"
 export BUCKET_NAME="arodriguezdevaw-bootcamp-2022-bucket" #username must be all lowercase
